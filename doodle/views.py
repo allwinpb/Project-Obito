9633261ef51e6f730b1677b60ddd7e3150415fa9
= <=>
2013-07-29 16:16:09 +0700

    changes

--- doodle/views.py
+++ doodle/views.py
@@ -1,17 +1,9 @@
 from doodle.models import GlobalChatHistory, Room, User, UserChatHistory
 from django.http import HttpResponse
-from django.shortcuts import render_to_response, render
-import datetime, urlparse
+from django.shortcuts import render_to_response
 
 def ChatHistory(request):
     return render_to_response("chathistory.html", {'history_list': UserChatHistory.objects.all()})
 
 def ChatHistoryIndex(request, history_id):
     return render_to_response("chatindex.html", {'history': UserChatHistory.objects.get(id=history_id)})
-
-def AddUser(request):
-    url = request.get_full_path()
-    par = urlparse.parse_qs(urlparse.urlparse(url).query)
-    User(id = par['id'], user = par['user'], join_date=datetime.datetime.now()).save()
-    return render(request, 'adduser.html')
-
--- web_server/web_server/urls.py
+++ web_server/web_server/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, include, url
-from doodle.views import ChatHistory, ChatHistoryIndex
+from doodle.views import ChatHistory, ChatHistoryIndex, AddUser
 
 urlpatterns = patterns('',
     url(r'^history/list/$', ChatHistory),
     url(r'^history/list/(?P<history_id>\d+)/$', ChatHistoryIndex),
+    url(r'^users/add/$', AddUser),
 )
