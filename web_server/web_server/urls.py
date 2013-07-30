00156faad9d38d32048fbab0333da3c16270ee9f
Allwin <allwinpb@gmail.com>
2013-07-31 01:42:01 +0530

    Added (non-functional) URL configs in urls.py. Need to validate and confirm

--- web_server/web_server/urls.py
+++ web_server/web_server/urls.py
@@ -4,7 +4,10 @@ from doodle.views import ChatHistory, ChatHistoryIndex, AddUser
 urlpatterns = patterns('',
     url(r'^history/list/$', ChatHistory),
     url(r'^history/list/(?P<history_id>\d+)/$', ChatHistoryIndex),
-    url(r'^users/add/$', AddUser),
+    url(r'^users/add/$', AddUser), #POST
     # url(r'/',HomePage),
     # url(r'/static/(?P<file_path>\s+)/$',StaticFileServer),
+    # url(r'^rooms/new$', RoomCreator),
+    # url(r'^rooms/(?P<room_id>\s+)/$', RoomServer),
+    # url(r'^/update$', MessageArchiver), #POST
 )
