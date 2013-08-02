af610926ed36a49d4cc4c268720374c1580d1029
= <=>
2013-08-02 21:49:42 +0700

    django_socket, you might want to look at it

--- web_server/web_server/urls.py
+++ web_server/web_server/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
-from doodle.views import ChatHistory, ChatHistoryIndex, AddUser, HomePage
+from doodle.views import ChatHistory, ChatHistoryIndex, AddUser, HomePage, StaticFileServer, RoomCreator, RoomServer, MessageArchiever
 
 urlpatterns = patterns('',
     url(r'^history/list/$', ChatHistory),
     url(r'^history/list/(?P<history_id>\d+)/$', ChatHistoryIndex),
     url(r'^users/add/$', AddUser), #POST
     url(r'^$',HomePage),
-    # url(r'/static/(?P<file_path>\s+)/$',StaticFileServer),
-    # url(r'^rooms/new$', RoomCreator),
-    # url(r'^rooms/(?P<room_id>\s+)/$', RoomServer),
-    # url(r'^/update$', MessageArchiver), #POST
+    url(r'/static/(?P<file_path>\s+)/$',StaticFileServer),
+    url(r'^rooms/new$', RoomCreator),
+    url(r'^rooms/(?P<room_id>\s+)/$', RoomServer),
+    url(r'^/update$', MessageArchiver), #POST
 )
