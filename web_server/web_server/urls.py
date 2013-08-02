4967ac2ad040bf4daa9e85f91b0858df3714b385
Allwin <allwinpb@gmail.com>
2013-08-02 23:00:12 +0530

    urls.py removed non-existent references

--- web_server/web_server/urls.py
+++ web_server/web_server/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from doodle.views import ChatHistory, ChatHistoryIndex, AddUser, HomePage, StaticFileServer, RoomCreator, RoomServer, MessageArchiever
+from doodle.views import ChatHistory, ChatHistoryIndex, AddUser, HomePage, RoomCreator, RoomServer, MessageArchiver
 
 urlpatterns = patterns('',
     url(r'^history/list/$', ChatHistory),
     url(r'^history/list/(?P<history_id>\d+)/$', ChatHistoryIndex),
     url(r'^users/add/$', AddUser), #POST
     url(r'^$',HomePage),
-    url(r'/static/(?P<file_path>\s+)/$',StaticFileServer),
     url(r'^rooms/new$', RoomCreator),
     url(r'^rooms/(?P<room_id>\s+)/$', RoomServer),
     url(r'^/update$', MessageArchiver), #POST
