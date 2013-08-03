c48b57b5f6a07ee37fe2ed443826f761d944b917
Allwin <allwinpb@gmail.com>
2013-08-04 00:32:45 +0530

    Fixed /update + /update/session to use port 8000

--- chat_server/chat_server.js
+++ chat_server/chat_server.js
@@ -87,7 +87,7 @@ io.sockets.on('connection', function(socket){
 		time_end = new Date();
 		//TODO: Send user chat history
 		if(userObj.usesFB==true){
-			bridge.post('http://localhost/update/session',{
+			bridge.post('http://localhost:8000/update/session',{
 				'user':userObj.userObj.id,
 				'id':room,
 				'join_time': time_join,
@@ -101,7 +101,7 @@ io.sockets.on('connection', function(socket){
 				console.log('Disassembling ROOM('+room+')...');
 				//send all the shit to sql for archiving
 				//django should delete all the associated room keys
-				bridge.post('http://localhost/update',{'id':room},function(error,resp,body){});
+				bridge.post('http://localhost:8000/update',{'id':room},function(error,resp,body){});
 			}
 		});
 		client.smembers("room:"+room+":users",function(err,reply){
